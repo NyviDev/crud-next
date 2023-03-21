da4c38ab75cd8570a15aba67721db8d13209dbc9
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-03-21 16:28:00 -0300

    feat: configura apikey do firebase no workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
     obterTodos,
   } = useClientes();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => obterTodos, []);
 
   return (
